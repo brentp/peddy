046dba96a53597f00cfba48fdef02bdb8438232a
Brent Pedersen <bpederse@gmail.com>
2016-08-17 10:57:20 -0600

    bump version

--- scripts/king-compare.sh
+++ scripts/king-compare.sh
@@ -7,5 +7,5 @@ set -ex
 VCF=$1
 prefix=$(basename $VCF .vcf.gz)
 echo $prefix
-/usr/bin/time plink2 --const-fid --allow-extra-chr --vcf $VCF --make-bed --out $prefix --biallelic-only --geno 0.95 --vcf-half-call m
+/usr/bin/time plink2 --const-fid --allow-extra-chr --vcf $VCF --make-bed --out $prefix --biallelic-only --geno 0.05 --vcf-half-call m
 /usr/bin/time king --ibs -b ${prefix}.bed --kinship --prefix $prefix
